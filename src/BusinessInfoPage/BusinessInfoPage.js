302e2fb9f0eaf89a71b64b48beefae54618ceaa3
Patrick Alexander <patrick_alexander@mail.com>
2021-05-10 15:13:06 -0500

    refactoring BusinessInfoPage inline css to seperate file

--- src/BusinessInfoPage/BusinessInfoPage.js
+++ src/BusinessInfoPage/BusinessInfoPage.js
@@ -39,30 +39,28 @@ function BusinessInfoPage({businessId}) {
     }
     
     return (
-        <section style={{minHeight:'100vh', paddingTop: '15vh', display:'grid', placeItems:'center', backgroundColor:'#feffe5', borderRadius:'5px'}}>
-                <div style={{ minHeight: '40vh', width:'auto'}}>
+        <section className='business'>
+                <div className='business__image'>
                     <div style={{height:'381px', backgroundSize: "cover", backgroundRepeat: 'no-repeat',width:'80%', margin:'0 auto 0 auto', backgroundImage: `url(${business.image_url})`, borderRadius:'5px'}}>
                 
                     </div>
                 </div>
-                <div style={{ minHeight: '5vh', width:'98vw'}}>
+                <div className='business__info' style={{ minHeight: '5vh', width:'98vw'}}>
                     <div style={{height:'100%', width:'80%', margin:'3vh auto 0 auto', backgroundColor:'white', padding: '20px'}}>
                     <h3>{business.name}</h3>
                         {/* <p>{(business.distance/1609.344).slice} mi away</p> */}
                         <p>{business.rating} stars</p>
                         <p>{business.display_phone}</p>
-                        <p>{business.location.display_address[0]}</p>
-                        <p>{business.location.display_address[1]}</p>
-                        <p>{business.location.display_address[2]}</p>
+                      
                     </div>
                 </div>
 
-                <div style={{ minHeight: '5vh', width:'98vw', marginBottom:'30px'}}>
-                    <div style={{height:'100%', width:'80%', margin:'0 auto 0 auto', marginTop: '3vh'}}>
+                <div className='business__review'>
+                    <div className='business__review--cta'>
                         <button className='button'> Write a Review!</button>
                     </div>
                 </div>
-                <div style={{ minHeight: '50vh', width:'98vw'}}>
+                <div className='business__review--list'>
                     <div style={{height:'100%', width:'80%', margin:'0 auto 0 auto'}}>
                         <div style={{ width:'100%', height:'100px', backgroundColor:'rgba(196, 196, 196, 0.3)', borderRadius:'5px', padding:'20px'}}>
                             <p style={{ fontFamily: 'Montserrat', fontweight:'600'}}>Review</p>
